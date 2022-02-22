34a86196fa546c0f331c536f5383a2d38ca5bef6
shtapv <shtapv@users.noreply.github.com>
2022-02-22 19:00:27 +0700

    geometry vkt

--- geometry.c
+++ geometry.c
@@ -1,42 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-const float PI = 3.1415926535897932384626433832795;
 int main()
 {
-	int a;
-	double a1, a2, a3, p, p2, rad, s;
-	printf("1. Circle\n2. Triangle\nChoose your figure: ");
-	scanf("%d", &a);
-	if (a == 1)
+	float x, y, z, s, p;
+	printf("Enter circle x,y and radius: ");
+	scanf(" circle(%f, %f, %f)", &x, &y, &z);
+	if (z > 0)
 	{
-		printf("Enter radius:");
-		scanf("%lf", &rad);
-		s = PI * rad * rad;
-		p = PI * 2 * rad;
-		printf("S circle: %.4lf\nLenght circle: %.4lf\n", s, p);
-	}
-	if (a == 2)
-	{
-
-		while (1)
-		{
-
-    	    		printf("Enter triangle sides: ");
-    	    		scanf("%lf%lf%lf", &a1, &a2, &a3);
-    	    		if (a3 <= a1 + a2)
-    	    		{
-	    	        	p = a1 + a2 + a3;
-	    	        	p2 = p / 2;
-		       		s = sqrt(p2 * (p2 - a1) * (p2 - a2) * (p2 - a3));
-		        	printf("S triangle: %.4lf\nPerimetr triangle: %.4lf\n", s, p);
-		        	break;
-    	    		}
-    	    		else
-    	    		{
-		  		printf("Error. ");
-    	        		continue;
-    	    		}
-		}
+		s = M_PI * z * z;
+		p = M_PI * z * 2;
+		printf("Perimetr: %f\nS: %f\n", s, p);
 	}
 	return 0;
 }
